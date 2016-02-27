281ad88fbc854a61b053879484a59146c4fdbc4b
Samuel Laska <samuel.laska@gmail.com>
2016-02-27 17:36:19 +0000

    Initial Commit

---
+++ Skyscanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skyscanner
+//
+//  Created by Samuel Laska on 2/27/16.
+//  Copyright © 2016 Samuel Laska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
